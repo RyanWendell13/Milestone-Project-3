7ccb1ebfb6a5d86b43e9ca22bef2a4f80def1154
Ryan Wendell <Ryan.wendell@me.com>
2022-11-23 16:05:54 -0800

    Added user context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,14 @@ import CategoryPage from './pages/CategoryPage';
 import RecipePage from './pages/RecipePage';
 import Error404 from './pages/Error404';
 
+import CurrentUserProvider from './contexts/CurrentUser';
+
 import './css/App.css';
 
 function App() {
   return (
-    <Router>
+    <CurrentUserProvider>
+      <Router>
         <Routes>
           <Route path='/' element={<CategoryPage/>}/>
           <Route path='/user' element={<UserPage/>}/>
@@ -16,6 +19,7 @@ function App() {
           <Route path='*' element={<Error404/>}/>
         </Routes>
       </Router>
+    </CurrentUserProvider>
   );
 }
 
