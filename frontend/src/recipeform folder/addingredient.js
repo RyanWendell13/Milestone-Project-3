74c30558f3168804648898354443b467497ea7d9
Your Name <you@example.com>
2022-11-30 05:03:07 -0800

    updated js and adding remove list element button

--- frontend/src/recipeform folder/addingredient.js
+++ frontend/src/recipeform folder/addingredient.js
@@ -3,61 +3,52 @@
 
 const addIng = document.querySelector('#add-info')
 const addIBtn = document.querySelector('#addIBtn')
+const ingList = document.querySelector('.add-ingredient-list')
+
 
 //create two variables (Direction list)
 const addDir = document.querySelector('.add-input')
 const addDBtn = document.querySelector('#addDBtn')
+const dirLst = document.querySelector('.add-direction-list')
+
 
 //create function for adding to Ingredient list
-function add2ListI() {
-    if (addIng.value ==='') {
-        alert('Enter Ingredient');
-    } else {
-        const ul = document.querySelector('ul');
-        const li = document.createElement('li');
-        li.innerHTML = addIng.value;
-        addIng.value = '';
-        ul.appendChild(li);
-        createBtn(li);
-  }
+addIBtn.addEventListener('click', (addI)=> {
+  if (addIng.value ==='') {
+    alert('Enter Ingredient');
+} else {
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    li.innerHTML = addIng.value;
+    ul.appendChild(li);
+    const listISpan = document.createElement('span')
+    listISpan.innerHTML = 'x';
+    li.appendChild(listISpan)
 }
-
+})
 //create function for adding to Direction list
-function add2ListD() {
+addDBtn.addEventListener('click', (addD) => {
   if (addDir.value ==='') {
       alert('Enter Cooking Steps');
   } else {
-      const ul = document.querySelector('ol');
-      const li = document.createElement('li');
-      li.innerHTML = addDir.value;
-      addDir.value = '';
-      ul.appendChild(li);
-      createBtn(li);
+    const ol = document.querySelector('ol');
+    const li = document.createElement('li');
+    li.innerHTML = addDir.value;
+    ol.appendChild(li);
+    const listDSpan = document.createElement('span')
+    listDSpan.innerHTML = 'x';
+    li.appendChild(listDSpan)
 }
-}
+})
 
-// add ingredient to list when button is clicked
-addIBtn.addEventListener('click', () => {
-    add2ListI();
-});
 
-// add direction to list when button is clicked
-addDBtn.addEventListener('click', () => {
-  add2ListD();
-});
-
-//add ingredient when enter is pressed
-//13 == enter button
-addIng.addEventListener('keyup', (event) => {
-    if(event.which === 13) {
-      add2ListI();
-    }
-  });
 
 //add direction when enter is pressed
 
-addDir.addEventListener('keyup', (event) => {
-  if(event.which === 13) {
-    add2ListD();
-  }
-});
+//addDBtn.addEventListener('keyup', (event) => {
+//  if(event.which === 13) {
+//    add2ListD();
+//  }
+//});
+
+
